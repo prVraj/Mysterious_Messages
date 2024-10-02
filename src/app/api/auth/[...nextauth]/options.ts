3260273f083bd7c4a1993a3ecd1adf64e1b8142d
Vraj Parikh <vrajparikh169@gmail.com>
2024-10-02 21:34:04 +0530

    all set up done with sign in

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,83 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/user.model";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "vrajpr873@mail.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials: any): Promise<any> {
+        await dbConnect();
+
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifier },
+              { userName: credentials.identifier },
+            ],
+          });
+          if (!user) {
+            throw new Error("No user found with this email or user name");
+          }
+
+          if (!user.isVerified) {
+            throw new Error("Please verify first befor signin");
+          }
+
+          const isCorrectPassword = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (isCorrectPassword) {
+            return user;
+          } else {
+            throw new Error("Incorrect password");
+          }
+        } catch (error: any) {
+          throw new Error(error);
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id;
+        session.user.isVerified = token.isVerified;
+        session.user.isMsgAccesspting = token.isMsgAccesspting;
+        session.user.userName = token.userName;
+      }
+
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString();
+        token.isVerified = user.isVerified;
+        token.isMsgAccesspting = user.isMsgAccesspting;
+        token.userName = user.userName;
+      }
+
+      return token;
+    },
+  },
+  pages: {
+    signIn: "/sign-in",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
---
+++ src/middleware.ts
@@ -0,0 +1,42 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { getToken } from 'next-auth/jwt'
+export { default } from "next-auth/middleware"
+
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: [
+    '/',
+    '/sign-in',
+    '/sign-out',
+    '/dashboard/:path*',
+    '/verify/:path*'
+  ]
+}
+
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+
+  const token = await getToken({req:request})
+  const url = request.nextUrl
+
+  if (token && 
+    (
+      url.pathname.startsWith('/sign-in') ||
+      url.pathname.startsWith('/sign-out') ||
+      url.pathname.startsWith('/verify') ||
+      url.pathname === ('/')
+    )
+  ) {    
+    return NextResponse.redirect(new URL('/dashboard', request.url))
+  }
+
+  if (!token && url.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/sign-in', request.url));
+  }
+
+  return NextResponse.next();
+  
+}
+ 
